5ba3286e9dd6d3784a3a1dbcf132b80aecbc14ce
rawsun007 <roshanramani.dev@gmail.com>
2025-01-07 18:53:56 +0530

    Added dynamic port binding for Render deployment

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from app.routes import app_routes
+import os  # To access environment variables
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS
@@ -10,4 +11,7 @@ app.register_blueprint(app_routes)
 
 if __name__ == "__main__":
     print("Starting Flask application...")
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    
+    # Use the PORT environment variable if defined; default to 5000 for local testing
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)

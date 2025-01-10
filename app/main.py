cce43c14b91dbb6ad3d16f0689cc7dd7f0c8892b
rawsun007 <roshanramani.dev@gmail.com>
2025-01-10 09:00:04 +0530

    update readme file

--- app/main.py
+++ app/main.py
@@ -3,15 +3,20 @@ from flask_cors import CORS
 from app.routes import app_routes
 import os  # To access environment variables
 
+# Initialize Flask app
 app = Flask(__name__)
-CORS(app)  # Enable CORS
 
-# Register routes
+# Enable CORS for all routes and origins (adjust this in production for security)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+# Register application routes
 app.register_blueprint(app_routes)
 
 if __name__ == "__main__":
     print("Starting Flask application...")
     
-    # Use the PORT environment variable if defined; default to 5000 for local testing
+    # Use the PORT environment variable if defined, default to 5000
     port = int(os.environ.get("PORT", 5000))
+    
+    # Run the Flask app
     app.run(debug=True, host="0.0.0.0", port=port)
